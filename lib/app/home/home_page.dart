a084eefa79c86f0712eaa99be9e5bce33c460f34
Claudio Moraes <77701146+crmoraes@users.noreply.github.com>
2022-04-10 16:36:55 -0400

    Added AllEntriesList

--- lib/app/home/home_page.dart
+++ lib/app/home/home_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:time_tracker_flutter_course/app/home/account/account_page.dart';
 import 'package:time_tracker_flutter_course/app/home/cupertino_home_scaffold.dart';
+import 'package:time_tracker_flutter_course/app/home/entries/entries_page.dart';
 import 'package:time_tracker_flutter_course/app/home/tab_item.dart';
 
 import 'jobs/jobs_page.dart';
@@ -25,7 +26,7 @@ class _HomePageState extends State<HomePage> {
   Map<TabItem, WidgetBuilder> get widgetBuilders {
     return {
       TabItem.jobs: (_) => const JobsPage(),
-      TabItem.entries: (_) => Container(),
+      TabItem.entries: (context) => EntriesPage.create(context),
       TabItem.account: (_) => const AccountPage(),
     };
   }
